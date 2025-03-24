f41734c255dcaabfe7cff6f23eecdf82f4eb23b0
Lingam <kjothilingam06@gmail.com>
2025-03-24 16:42:57 +0530

    component added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,4 +6,24 @@ export const routes: Routes = [
         path:'',
         loadComponent :()=>import('./components/login/login.component').then((c)=> c.LoginComponent),
     },
+    {
+        path:'dashboard',
+        loadComponent :()=>import('./components/dashboard/dashboard.component').then((c)=> c.DashboardComponent),
+    },
+    {
+        path:'report',
+        loadComponent :()=>import('./components/report/report.component').then((c)=> c.ReportComponent),
+    },
+    {
+        path:'profile',
+        loadComponent :()=>import('./components/profile/profile.component').then((c)=> c.ProfileComponent),
+    },
+    {
+        path:'adduser',
+        loadComponent :()=>import('./components/add-user/add-user.component').then((c)=> c.AddUserComponent),
+    },
+    {
+        path:'generatebill',
+        loadComponent :()=>import('./components/generate-bill/generate-bill.component').then((c)=> c.GenerateBillComponent),
+    },
 ];
